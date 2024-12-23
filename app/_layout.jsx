4373c083204e76736db6ee24775ca3abda8c22a6
Mani0919 <tangudumanikanta63@gmail.com>
2024-12-23 15:29:26 +0530

    push notifications for android

---
+++ app/_layout.jsx
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { NotificationProvider } from "../context/notification";
+import * as Notifications from "expo-notifications";
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function Root() {
+  return (
+    <>
+      <NotificationProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </NotificationProvider>
+    </>
+  );
+}
